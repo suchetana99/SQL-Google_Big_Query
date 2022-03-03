21450c38c4b92d29914bc7e5d9febd5ab666a7dc
Suchetana Roy <100765828+suchetana99@users.noreply.github.com>
2022-03-04 00:14:08 +0530

    Update iowa_liquor_sales.sql

--- iowa_liquor_sales.sql
+++ iowa_liquor_sales.sql
@@ -1,6 +1,6 @@
 --iowa_liquor_sales
 
---what is the proportion of revenue sales greater than 22000 dollars earned by vendrs in the county Scott?
+--what is the proportion of revenue sales greater than 22000 dollars earned by vendors in the county Scott?
 
 SELECT  DISTINCT vendor_name	, sale_dollars   FROM `bigquery-public-data.iowa_liquor_sales.sales` 
 WHERE LOWER(county)="scott" and sale_dollars > 22000
