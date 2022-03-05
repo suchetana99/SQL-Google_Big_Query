c269b14db7250016d99834fe14b9c36c42a1c630
Suchetana Roy <100765828+suchetana99@users.noreply.github.com>
2022-03-05 12:51:22 +0530

    Update fips2.sql

--- fips2.sql
+++ fips2.sql
@@ -1,5 +1,7 @@
 --fips2.sql
 
+-- What are the total employments in the respective counties where the net earnings is greater than 80000 as per residence?
+
 SELECT GeoName,Total_employment FROM `bigquery-public-data.sdoh_bea_cainc30.fips`
 WHERE Net_earnings_by_place_of_residence>80000
 ORDER BY 2
